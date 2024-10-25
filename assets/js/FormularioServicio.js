b584086d588065f7a1804f4a3baf6548dc9b24a7
maximiliano <maxicaprioglio@gmail.com>
2024-10-25 16:27:07 -0300

    Modificaciones JS

--- assets/js/FormularioServicio.js
+++ assets/js/FormularioServicio.js
@@ -1,197 +1,174 @@
 export class FormularioServicio {
-    constructor(
+  constructor(nombre, email, ubicacion, cantidad, servicio, message) {
+    this.nombre = nombre;
+    this.email = email;
+    this.ubicacion = ubicacion;
+    this.cantidad = cantidad;
+    this.servicio = servicio;
+    this.message = message;
+  }
+
+  solicitarDatos() {
+    const nombre = this.SolicitarNombre();
+    const email = this.SolicitarEmail();
+    const ubicacion = this.SolicitarUbicacion();
+    const cantidad = this.SolicitarCantidad();
+    const servicio = this.SolicitarServicio();
+    const message = prompt("Mensaje:");
+
+    let datosFormulario = new FormularioServicio(
       nombre,
       email,
       ubicacion,
       cantidad,
-      entrada,
-      parrilla,
-      vegetariano,
-      pescados,
-      minutas,
-      pizzas,
+      servicio,
       message
-    ) {
-      this.nombre = nombre;
-      this.email = email;
-      this.ubicacion = ubicacion;
-      this.cantidad = cantidad;
-      this.entrada = entrada;
-      this.parrilla = parrilla;
-      this.vegetariano = vegetariano;
-      this.pescados = pescados;
-      this.minutas = minutas;
-      this.pizzas = pizzas;
-      this.message = message;
-    }
-  
-    SolicitarDatos() {
-      let scope;
-      let valorNombre = "";
-      do {
-        valorNombre = prompt("Ingrese Su nombre: ");
-        if (valorNombre == "") {
-          alert("Ingrese un valor, intentemos devuetla.");
-        }
-      } while (valorNombre == "");
-  
-      let valorEmail = "";
-      do {
-        valorEmail = prompt("Ingrese Su mail: ");
-        var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (regex.test(valorEmail)) {
-          scope = true;
-        } else {
-          scope = false;
-          alert("No es un mail, intentemos devuelta.");
-        }
-      } while (!scope);
-  
-      let valorUbicacion;
-      do {
-        valorUbicacion = prompt(
-          "Ingrese el numero de su ubicacion: 1 - caba , 2 - norte, 3 - sur, 4 - oeste, 5 - mas de 15km"
-        );
-        switch (valorUbicacion) {
-          case "1":
-            valorUbicacion = "caba";
-            scope = true;
-            break;
-          case "2":
-            valorUbicacion = "norte";
-            scope = true;
-            break;
-          case "3":
-            valorUbicacion = "sur";
-            scope = true;
-            break;
-          case "4":
-            valorUbicacion = "oeste";
-            scope = true;
-            break;
-          case "4":
-            valorUbicacion = "mas";
-            scope = true;
-            break;
-          default:
-            alert("Valor incorrecto, intentemos devuelta");
-            scope = false;
-            break;
-        }
-      } while (!scope);
-  
-      let valorCantidad;
-      do {
-        valorCantidad = prompt("Cuantos comensales son:");
-        if (valorCantidad === "") {
-          alert("No pusiste nada, intentemos devuelta");
-  
-          scope = false;
-        } else if (isNaN(valorCantidad)) {
-          scope = false;
-          alert("no es un numero, intentemos devuelta");
-        } else {
-          valorCantidad = Number(valorCantidad);
-          scope = true;
-        }
-      } while (!scope);
-  
-      let valorEntrada = prompt(
-        "El servicio viene con: entrada? no 'precione enter', si 'marque algo y presione enter' "
-      );
-      if (valorEntrada === "") {
-        valorEntrada = false;
-      } else {
-        valorEntrada = true;
-      }
-  
-      let valorParrilla = prompt(
-        "El servicio viene con: parrilla? no 'precione enter', si 'marque algo y presione enter' "
-      );
-      if (valorParrilla === "") {
-        valorParrilla = false;
-      } else {
-        valorParrilla = true;
-      }
-  
-      let valorVegetariano = prompt(
-        "El servicio viene con: Vegetariano? no 'precione enter', si 'marque algo y presione enter' "
-      );
-      if (valorVegetariano === "") {
-        valorVegetariano = false;
-      } else {
-        valorVegetariano = true;
-      }
-  
-      let valorPescados = prompt(
-        "El servicio viene con: Pescados? no 'precione enter', si 'marque algo y presione enter' "
-      );
-      if (valorPescados === "") {
-        valorPescados = false;
-      } else {
-        valorPescados = true;
-      }
-  
-      let valorMinutas = prompt(
-        "El servicio viene con: Minutas? no 'precione enter', si 'marque algo y presione enter' "
-      );
-      if (valorMinutas === "") {
-        valorMinutas = false;
-      } else {
-        valorMinutas = true;
-      }
-  
-      let valorPizzas = prompt(
-        "El servicio viene con: Pizzas? no 'precione enter', si 'marque algo y presione enter' "
-      );
-      if (valorPizzas === "") {
-        valorPizzas = false;
-      } else {
-        valorPizzas = true;
-      }
-  
-      let valorMessage = prompt("Mensaje:");
-  
-      let datosFormulario = new FormularioServicio(
-        valorNombre,
-        valorEmail,
-        valorUbicacion,
-        valorCantidad,
-        valorEntrada,
-        valorParrilla,
-        valorVegetariano,
-        valorPescados,
-        valorMinutas,
-        valorPizzas,
-        valorMessage
-      );
-      alert("se cargo correctamente");
-      return datosFormulario;
-    }
-    MostrarMensaje() {
-      alert(
-        "Los datos guardados son: nombre " +
-          this.nombre +
-          " email " +
-          this.email +
-          " ubicacion " +
-          this.ubicacion +
-          " cantidad " +
-          this.cantidad +
-          " entrada " +
-          this.entrada +
-          " parrilla " +
-          this.parrilla +
-          " vegetariano " +
-          this.vegetariano +
-          " pescados " +
-          this.pescados +
-          " minutas " +
-          this.minutas +
-          " pizzas " +
-          this.pizzas +
-          " message " +
-          this.message
-      );
-    }
+    );
+    alert("se cargó correctamente");
+
+    return datosFormulario;
   }
+
+  SolicitarNombre() {
+    let valorNombre = "";
+    do {
+      valorNombre = prompt("Ingrese Su nombre: ");
+      if (valorNombre == "") {
+        alert("Ingrese un valor, intentemos devuetla.");
+      }
+    } while (valorNombre == "");
+    return valorNombre;
+  }
+
+  SolicitarEmail() {
+    let scope;
+    let valorEmail = "";
+    do {
+      valorEmail = prompt("Ingrese Su mail: ");
+      var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (regex.test(valorEmail)) {
+        scope = true;
+      } else {
+        scope = false;
+        alert("No es un mail, intentemos devuelta.");
+      }
+    } while (!scope);
+    return valorEmail;
+  }
+
+  SolicitarUbicacion() {
+    let scope;
+    let valorUbicacion;
+    do {
+      valorUbicacion = prompt(
+        "Ingrese el numero de su ubicacion: 1 - caba , 2 - norte, 3 - sur, 4 - oeste, 5 - mas de 15km"
+      );
+      switch (valorUbicacion) {
+        case "1":
+          valorUbicacion = "caba";
+          scope = true;
+          break;
+        case "2":
+          valorUbicacion = "norte";
+          scope = true;
+          break;
+        case "3":
+          valorUbicacion = "sur";
+          scope = true;
+          break;
+        case "4":
+          valorUbicacion = "oeste";
+          scope = true;
+          break;
+        case "4":
+          valorUbicacion = "mas";
+          scope = true;
+          break;
+        default:
+          alert("Valor incorrecto, intentemos devuelta");
+          scope = false;
+          break;
+      }
+    } while (!scope);
+    return valorUbicacion;
+  }
+
+  SolicitarCantidad() {
+    let scope;
+    let valorCantidad;
+    do {
+      valorCantidad = prompt("Cuantos comensales son:");
+      if (valorCantidad === "") {
+        alert("No pusiste nada, intentemos devuelta");
+
+        scope = false;
+      } else if (isNaN(valorCantidad)) {
+        scope = false;
+        alert("no es un numero, intentemos devuelta");
+      } else {
+        valorCantidad = Number(valorCantidad);
+        scope = true;
+      }
+    } while (!scope);
+    return valorCantidad;
+  }
+
+  SolicitarServicio() {
+    const valorServicio = [];
+    let valorEntrada = prompt(
+      "El servicio viene con: entrada? no 'precione enter', si 'marque algo y presione enter' "
+    );
+    if (valorEntrada !== "") {
+      valorServicio.push("Entrada");
+    }
+
+    let valorParrilla = prompt(
+      "El servicio viene con: parrilla? no 'precione enter', si 'marque algo y presione enter' "
+    );
+    if (valorParrilla !== "") {
+      valorServicio.push("Parrilla");
+    }
+
+    let valorVegetariano = prompt(
+      "El servicio viene con: Vegetariano? no 'precione enter', si 'marque algo y presione enter' "
+    );
+    if (valorVegetariano !== "") {
+      valorServicio.push("Vegetariano");
+    }
+
+    let valorPescados = prompt(
+      "El servicio viene con: Pescados? no 'precione enter', si 'marque algo y presione enter' "
+    );
+    if (valorPescados !== "") {
+      valorServicio.push("Pescados");
+    }
+
+    let valorMinutas = prompt(
+      "El servicio viene con: Minutas? no 'precione enter', si 'marque algo y presione enter' "
+    );
+    if (valorMinutas !== "") {
+      valorServicio.push("Minutas");
+    }
+
+    let valorPizzas = prompt(
+      "El servicio viene con: Pizzas? no 'precione enter', si 'marque algo y presione enter' "
+    );
+    if (valorPizzas !== "") {
+      valorServicio.push("Pizzas");
+    }
+    return valorServicio;
+  }
+
+  mostrarDatos() {
+    const servicios = this.servicio.join(", ");
+    alert(
+      `Datos del formulario:
+    Nombre: ${this.nombre}
+    Email: ${this.email}
+    Ubicación: ${this.ubicacion}
+    Cantidad: ${this.cantidad}
+    Servicio: ${servicios}
+    Mensaje: ${this.message}`
+    );
+  }
+}
