fae209e56c77235f5348c839935ca4347e908cc3
maximiliano <maxicaprioglio@gmail.com>
2024-11-19 14:29:41 -0300

    cuarta entrega

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,27 @@
-import { Carrito } from "./Carrito.js";
-import { Producto } from "./Producto.js";
-import { FormularioServicio } from "./FormularioServicio.js";
+import {
+  agregarClickBotonesEnviar,
+  agregarClickBotonesSumar,
+  agregarClickBotonesRestar,
+} from "./buttonActions.js";
 
-const carrito = new Carrito();
-const producto1 = new Producto(1,"imagen01.jpg" ,"pastel", "Un pastel enorme con todo lo que se necesita",25000, 5);
-const producto2 = new Producto(2,"imagen01.jpg" ,"pastel", "Un pastel enorme con todo lo que se necesita",25000, 5);
+import { cargarCantCarrito, cargarProductosTabla } from "./Carrito.js";
 
+// viandas
+cargarCantCarrito();
+agregarClickBotonesEnviar();
+cargarProductosTabla(JSON.parse(localStorage.getItem("carrito")) ?? []);
+agregarClickBotonesRestar();
+agregarClickBotonesSumar();
 
-carrito.agregarProducto(producto1);
-carrito.mostrarProductos();
-carrito.agregarProducto(producto2);
-carrito.mostrarProductos();
-carrito.agregarProducto(new Producto(1,"imagen01.jpg" ,"pastel", "Un pastel enorme con todo lo que se necesita",25000, 5));
-carrito.mostrarProductos();
-carrito.descontarProducto(2);
-carrito.mostrarProductos();
-carrito.descontarProducto(2);
-carrito.mostrarProductos();
-carrito.descontarProducto(2);
-carrito.mostrarProductos();
-carrito.descontarProducto(2);
-carrito.mostrarProductos();
-carrito.descontarProducto(2);
-carrito.mostrarProductos();
-carrito.eliminarProducto(2);
-carrito.mostrarProductos();
-carrito.eliminarProducto(1);
-carrito.mostrarProductos();
+// formulario servicios
 
-console.log("bienvenido, le solicitamos datos del formulario!");
-let datos = new FormularioServicio().solicitarDatos();
-datos.mostrarDatos();
+document.getElementById("formulario").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  let data = [];
+  for (let [key, value] of formData.entries()) {
+    data.push(key, value);
+  }
+  console.log(data);
+  // aca hay que hacer un fetch y mandarlo a algun lado
+});
